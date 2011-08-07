4de0c3e07b56071529157ecdb158ddff95621e16
kallt_kaffe <kallt_kaffe@apedroid.com>
2011-08-07 23:34:02 +0200

    Basic screen calibration. Not the final version and values are reset on reboot but it's a start.

--- V9Parts/java/com/cyanogenmod/V9Parts/V9PartsStartup.java
+++ V9Parts/java/com/cyanogenmod/V9Parts/V9PartsStartup.java
@@ -3,18 +3,33 @@ package com.cyanogenmod.V9Parts;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class V9PartsStartup extends BroadcastReceiver
 {
-    private static final String TAG = "V9PartsStartup";
-    @Override
-    public void onReceive(final Context context, final Intent bootintent) {
-	// Here we will write calibration values to /sys/module/msm_ts/parameters/
-    }
+   private void writeValue(String parameter, int value) {
+      try {
+          FileOutputStream fos = new FileOutputStream(new File("/sys/module/msm_ts/parameters/tscal_" + parameter));
+          fos.write(String.valueOf(value).getBytes());
+          fos.flush();
+          fos.getFD().sync();
+          fos.close();
+      } catch (FileNotFoundException e) {
+         e.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+
+   @Override
+   public void onReceive(final Context context, final Intent bootintent) {
+      // Here we write calibration values to /sys/module/msm_ts/parameters/
+      writeValue("xoffset",-1703937);
+      writeValue("yoffset",1);
+      writeValue("xscale",34876);
+      writeValue("yscale",58126);
+   }
 }
