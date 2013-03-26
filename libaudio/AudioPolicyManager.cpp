4503806b91ab1a05d4b954d4bd56fd9751578450
Grigori Goronzy <greg@blackbox>
2013-03-26 18:45:57 +0100

    libaudio: fix FM stream refcount
    refCount() reports the sum of refcounts for *all* streams. So switching
    off FM did not work if another stream was active. Oops.
    
    Change-Id: I35f538f099a434fb97925a0203da85ae657aed82

--- libaudio/AudioPolicyManager.cpp
+++ libaudio/AudioPolicyManager.cpp
@@ -403,12 +403,12 @@ status_t AudioPolicyManager::setDeviceConnectionState(AudioSystem::audio_devices
             AudioOutputDescriptor *out = mOutputs.valueFor(mPrimaryOutput);
             if (state == AudioSystem::DEVICE_STATE_AVAILABLE) {
                 out->changeRefCount(AudioSystem::FM, 1);
-                if (out->refCount() > 0)
+                if (out->mRefCount[AudioSystem::FM] > 0)
                     mpClientInterface->setParameters(0, String8("fm_on=1"));
             }
             else {
                 out->changeRefCount(AudioSystem::FM, -1);
-                if (out->refCount() <= 0)
+                if (out->mRefCount[AudioSystem::FM] <= 0)
                     mpClientInterface->setParameters(0, String8("fm_off=1"));
             }
         }
