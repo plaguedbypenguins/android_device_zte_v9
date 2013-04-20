bd2720a1cd52c7121b688af590761d4cbb7ddc15
Robin Humble <plaguedbypenguins@gmail.com>
2013-04-20 23:46:29 +1000

    sync with blade: reorganize and cleanup makefiles
    *move ramdisk
    *simplify copying prebuilt
    *reorganize makefiles
    *remove unneeded makefiles
    
    from konstat commit 8f7b61d6e095eec6a2a930f8b22f3f98146a0225

--- cm.mk
+++ cm.mk
@@ -1,17 +1,23 @@
-# Correct bootanimation size for the screen
-TARGET_SCREEN_HEIGHT := 800
-TARGET_SCREEN_WIDTH := 480
-
-$(call inherit-product, device/zte/v9/v9.mk)
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Inherit device configuration
+$(call inherit-product, device/zte/v9/v9.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=v9 BUILD_ID=JRO03C BUILD_FINGERPRINT=ZTE/N880E_JB4_2/atlas40:4.2/JOP40C/20121121.110335:user/release-keys PRIVATE_BUILD_DESC="N880E_JB4_2-user 4.2 JOP40C 20121121.110335 release-keys" BUILD_NUMBER=20121121.110335
+# Correct bootanimation size for the screen
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
 
-PRODUCT_NAME := cm_v9
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := v9
+PRODUCT_NAME := cm_v9
+PRODUCT_BRAND := ZTE
+PRODUCT_MODEL := V9
+PRODUCT_MANUFACTURER := ZTE
 PRODUCT_RELEASE_NAME := V9
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=v9 BUILD_ID=JRO03C BUILD_FINGERPRINT=ZTE/N880E_JB4_2/atlas40:4.2/JOP40C/20121121.110335:user/release-keys PRIVATE_BUILD_DESC="N880E_JB4_2-user 4.2 JOP40C 20121121.110335 release-keys" BUILD_NUMBER=20121121.110335
--- v9.mk
+++ v9.mk
@@ -1,35 +1,100 @@
-# Copyright (C) 2011 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-#
-# This file is the build configuration for a full Android
-# build for toro hardware. This cleanly combines a set of
-# device-specific aspects (drivers) with a device-agnostic
-# product configuration (apps). Except for a few implementation
-# details, it only fundamentally contains two inherit-product
-# lines, full and toro, hence its name.
-#
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
-# Inherit from v9 device
-$(call inherit-product, device/zte/v9/device.mk)
+$(call inherit-product-if-exists, vendor/zte/v9/v9-vendor.mk)
 
-# Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := v9
+DEVICE_PACKAGE_OVERLAYS := device/zte/v9/overlay
+
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+# Graphics
+PRODUCT_PACKAGES += \
+    gralloc.msm7x27 \
+    hwcomposer.msm7x27 \
+    copybit.msm7x27
+
+# OMX
+PRODUCT_PACKAGES += \
+    libmm-omxcore \
+    libOmxCore \
+    libstagefrighthw
+
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm7x27
+
+# GPS
+PRODUCT_PACKAGES += \
+    librpc \
+    gps.v9
+
+# V9 specific
+PRODUCT_PACKAGES += \
+    lights.v9 \
+    sensors.v9 \
+    V9Parts
+
+# Power HAL
+PRODUCT_PACKAGES += \
+    power.msm7x27
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.primary.v9 \
+    audio_policy.v9 \
+    audio.a2dp.default \
+    libaudioutils
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+    LiveWallpapersPicker \
+    librs_jni
+
+# FM Radio
+PRODUCT_PACKAGES += \
+    libfmradio.si4708 \
+    Effem
+
+# Other
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs \
+    dexpreopt
+
+# Enable repeatable keys in CWM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.cwm.enable_key_repeat=true
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    device/zte/v9/ramdisk/init.v9.rc:root/init.v9.rc \
+    device/zte/v9/ramdisk/init.v9.usb.rc:root/init.v9.usb.rc \
+    device/zte/v9/ramdisk/ueventd.v9.rc:root/ueventd.v9.rc
+
+# Prebuilt
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,device/zte/v9/prebuilt/system,system)
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/com.stericsson.hardware.fm.receiver.xml:system/etc/permissions/com.stericsson.hardware.fm.receiver.xml
+
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_v9
 PRODUCT_DEVICE := v9
-PRODUCT_BRAND := ZTE
 PRODUCT_MANUFACTURER := ZTE
 PRODUCT_MODEL := V9
